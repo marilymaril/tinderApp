a99f724c1e5409015e55fe72d0e2e576f4245551
marilymaril <mflorek@csumb.edu>
2018-10-29 18:04:23 -0700

    Initial Commit

---
+++ tinderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tinderApp
+//
+//  Created by Marilyn Florek on 10/29/18.
+//  Copyright © 2018 Marilyn Florek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
